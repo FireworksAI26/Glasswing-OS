ac42a0b02c2e5b14817e0e1364633938db5a0a89
AlexanderMum <makeably@atomicmail.io>
2026-06-26 02:35:49 +0000

    Zorin: switch display backend to TigerVNC for GNOME Shell (start.sh)

--- zorin/start.sh
+++ zorin/start.sh
@@ -17,7 +17,8 @@ export WIDTH HEIGHT DEPTH DISPLAY XDG_RUNTIME_DIR HOME=/home/zorin USER=zorin
 
 install -d -o zorin -g zorin -m 0700 "$XDG_RUNTIME_DIR"
 install -d -o zorin -g zorin -m 0700 /home/zorin/.vnc
-printf '%s' "$VNC_PASSWORD" | x11vnc -storepasswd - /home/zorin/.vnc/passwd >/dev/null
+# TigerVNC-compatible password file (DES VNC auth)
+printf '%s\n' "$VNC_PASSWORD" | vncpasswd -f > /home/zorin/.vnc/passwd
 chown zorin:zorin /home/zorin/.vnc/passwd
 chmod 0600 /home/zorin/.vnc/passwd
 printf 'zorin:%s\n' "$VNC_PASSWORD" | chpasswd
